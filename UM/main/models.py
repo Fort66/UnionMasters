755840124776ff7493a99d645a83f83abd886100
Vladislav-Shi <vlad.shirobockow@yandex.ru>
2023-03-09 20:04:04 +0300

    test commit

--- UM/main/models.py
+++ UM/main/models.py
@@ -4,6 +4,7 @@ from django.db.models import ImageField, ForeignKey
 
 class Cities(models.Model):
     CityName = models.CharField(max_length=100, blank=True, null=True, verbose_name='Название города')
+
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
@@ -18,11 +19,12 @@ class Users(models.Model):
     UserLastName = models.CharField(max_length=100, blank=True, null=True, verbose_name='Фамилия')
     UserPhone = models.CharField(max_length=100, null=False, verbose_name='Телефон')
     UserAddress = models.CharField(max_length=200, null=False, verbose_name='Адрес')
-    #UserApartment = models.CharField(max_length=100, blank=True, null=True)
+    # UserApartment = models.CharField(max_length=100, blank=True, null=True)
     UserIntercomCode = models.CharField(max_length=100, blank=True, null=True, verbose_name='Код домофона')
     UserEmail = models.CharField(max_length=100, blank=True, null=True, verbose_name='Электронная почта')
     UserCityId = models.ForeignKey(Cities, on_delete=models.CASCADE, verbose_name='Ссылка на город')
-    #UserStreetsId = models.ForeignKey('Streets', on_delete=models.CASCADE)
+
+    # UserStreetsId = models.ForeignKey('Streets', on_delete=models.CASCADE)
     # UserCitiesId = models.IntegerField()
     # UserStreetsId = models.IntegerField()
     class Meta:
@@ -36,6 +38,7 @@ class Users(models.Model):
 
 class Technics(models.Model):
     TechnicName = models.CharField(max_length=100, null=False, verbose_name='Наименование техники')
+
     class Meta:
         verbose_name = 'Техника'
         verbose_name_plural = 'Техника'
@@ -50,6 +53,7 @@ class Orders(models.Model):
     OrderDate = models.DateTimeField(auto_now_add=True, verbose_name='Дата заказа')
     UserId = ForeignKey(Users, on_delete=models.CASCADE, verbose_name='Ссылка на пользователя')
     OrderTechnicsId = ForeignKey(Technics, on_delete=models.CASCADE, verbose_name='Ссылка на технику')
+
     # UserId = models.IntegerField()
     # OrderTechnicsId = models.IntegerField()
     class Meta:
@@ -57,13 +61,11 @@ class Orders(models.Model):
         verbose_name_plural = 'Заказы'
         ordering = ['-OrderDate']
 
-    
 
-
-#class Streets(models.Model):
-    #StreetName = models.CharField(max_length=100, blank=True, null=True)
-    #StreetCitiesId = ForeignKey(Cities, on_delete=models.CASCADE)
-    # StreetCitiesId = models.IntegerField()
+# class Streets(models.Model):
+# StreetName = models.CharField(max_length=100, blank=True, null=True)
+# StreetCitiesId = ForeignKey(Cities, on_delete=models.CASCADE)
+# StreetCitiesId = models.IntegerField()
 
 
 class LinksTable(models.Model):
@@ -77,6 +79,7 @@ class LinksTable(models.Model):
     ViberLink = models.CharField(max_length=100, blank=True, null=True, verbose_name='Ссылка на Viber')
     TwoGisIcon = models.ImageField(upload_to='TwoGisIcon', blank=True, null=True, verbose_name='Иконка 2Gis')
     TwoGisLink = models.CharField(max_length=100, blank=True, null=True, verbose_name='Ссылка на 2Gis')
+
     class Meta:
         verbose_name = 'Ссылка на соцсети'
         verbose_name_plural = 'Ссылки на соцсети'
@@ -97,57 +100,72 @@ class Owner(models.Model):
     OwnerEmail = models.CharField(max_length=100, blank=True, null=True, verbose_name='Электронная почта')
     OwnerLegalAddress = models.CharField(max_length=100, blank=True, null=True, verbose_name='Адрес')
     OwnerCityId = ForeignKey(Cities, on_delete=models.CASCADE, verbose_name='Ссылка на город')
+
     class Meta:
         verbose_name = 'Владелец'
         verbose_name_plural = 'Владельцы'
         ordering = ['id']
 
+
 class MainIndexMedia(models.Model):
     photo = models.ImageField(upload_to='MainPageMedia', blank=True, null=True, verbose_name='Фото')
+
     class Meta:
         verbose_name = 'Услуги Фото'
         verbose_name_plural = 'Услуги Фото'
 
+
 class MainIndexChapterOne(models.Model):
     title = models.TextField(verbose_name='Заголовок')
+
     class Meta:
         verbose_name = 'Услуги левая колонка'
         verbose_name_plural = 'Услуги левая колонка'
         ordering = ['id']
 
+
 class MainIndexChapterTwo(models.Model):
     title = models.TextField(verbose_name='Заголовок')
+
     class Meta:
         verbose_name = 'Услуги правая колонка'
         verbose_name_plural = 'Услуги правая колонка'
         ordering = ['id']
-        
+
+
 class MainIndexAdditionService(models.Model):
     title = models.TextField(verbose_name='Заголовок')
+
     class Meta:
         verbose_name = 'Услуги доп услуги'
         verbose_name_plural = 'Услуги доп услуги'
         ordering = ['id']
-        
+
+
 class AboutUs(models.Model):
     descriptions = models.TextField(verbose_name='Описание')
+
     class Meta:
         verbose_name = 'О нас'
         verbose_name_plural = 'О нас'
         ordering = ['id']
 
+
 class LogoSite(models.Model):
     logo = models.ImageField(upload_to='LogoSite', blank=True, null=True, verbose_name='Логотип')
     favicon = models.ImageField(upload_to='favicon', blank=True, null=True, verbose_name='Иконка')
+
     class Meta:
         verbose_name = 'Логотип'
         verbose_name_plural = 'Логотипы'
         ordering = ['id']
 
+
 class UsersReviews(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='Пользователь')
     review = models.TextField(verbose_name='Отзыв')
     ReviewDate = models.DateTimeField(auto_now_add=True, verbose_name='Дата отзыва')
+
     class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
